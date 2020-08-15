8055b03e066021a32b4c0321025959b5a57a47a8
ducnt114 <trungduc110492@gmail.com>
2020-08-15 11:34:24 +0700

    Update project structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './react/App';
+import Profile from './react/Profile';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <BrowserRouter>
+      <div className="App">
+        <Route path="/" exact component={App} />
+        <Route path="/Profile" exact component={Profile} />
+      </div>
+    </BrowserRouter> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
